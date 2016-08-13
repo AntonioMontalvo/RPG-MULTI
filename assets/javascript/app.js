845b3d63ec5b1a3897e40681894f39bc7d5219ef
Antonio Montalvo <montalvocode@yahoo.com>
2016-08-12 17:08:59 -0700

    basic color template

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,15 @@
+
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBrisdASLvt24maDABwnxzDuB0a-Et3c1s",
+    authDomain: "rpg-multiplayer-70b63.firebaseapp.com",
+    databaseURL: "https://rpg-multiplayer-70b63.firebaseio.com",
+    storageBucket: "rpg-multiplayer-70b63.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+
+
 console.log('hello');
 var test = $('<div>');
 test.attr('class', 'test2');
